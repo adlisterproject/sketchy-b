a85dfb407b9884a68040e9df589a1d0954901511
Mary Warren <Friday@Marys-MacBook-Air.local>
2015-12-14 14:26:52 -0600

    added basic file structure to each page

---
+++ public/ads.edit.php
@@ -0,0 +1,14 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<?php require_once('../views/header.php') ?> 
+<?php require_once('../views/navbar.php') ?>
+
+<body>
+	
+</body>
+
+<?php require_once('../views/footer.php') ?>
+</html>
---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<?php require_once('../views/header.php') ?> 
+<?php require_once('../views/navbar.php') ?>
+
+<body>
+	<h1>Welcome to Sketchy-B!</h1>
+	<div id = "user_create">
+		<a href="user.create.php">Create a Profile!</a>
+	</div>
+	
+</body>
+
+<?php require_once('../views/footer.php') ?>
+</html>
+
+
