045bbec99e552390036cdc5cf76cdf196b52cc29
Alejandra Ramirez <alejandraramirez@Alejandra-Ramirezs-MacBook-Pro.local>
2015-12-14 14:31:14 -0600

    ADDED navbar, ads.index, and migration

---
+++ public/ads.index.php
@@ -0,0 +1,14 @@
+<?php
+?>
+
+<html>
+<head>
+	<?= require "../public/navbar.php";
+	?>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
+
---
+++ views/navbar.php
@@ -0,0 +1,16 @@
+
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+  	<ul class="nav navbar-nav">
+     	<li><a href="/..index.php">Home</a></li>
+     	<li><a href="/..ads.index.php">List All Adds</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+    	<li><a href="/..users.show.php">Profile</a></li>
+    	<li><a href="/..auth.logout.php">Log Out</a></li>
+    	<li><a href="/..auth.login.php">Sign In</a></li>
+    </ul>
+  </div>
+</nav>
+
