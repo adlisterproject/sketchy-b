2cafb21050a281c3f49a8093861fc20edc218507
Alejandra Ramirez <aer3934@gmail.com>
2015-12-15 10:57:06 -0600

    Updated gitignore

--- public/ads.create.php
+++ public/ads.create.php
@@ -1,4 +1,27 @@
 <?php
+
+function insertads($dbc){
+
+	$insert = "INSERT INTO adlister (item_name, price, description, contact)
+	VALUES (:item_name, :price, :description, :contact)";
+	$stmt->bindValue(':item_name', $ad['item_name'], PDO::PARAM_STR);
+	$stmt->bindValue(':price', $ad['price'], PDO::PARAM_STR);
+	$stmt->bindValue(':description', $ad['description'], PDO::PARAM_STR);
+	$stmt->bindValue(':contact', $ad['contact'], PDO::PARAM_STR);
+	$stmt->execute();
+}
+
+if (!empty($_POST)){
+	if (Input::noInput('name') &&
+		Input::noInput('location') &&
+		Input::noInput('date_established') &&
+		Input::noInput('area_in_acres') &&
+		Input::noInput('description') 
+		) {
+
+		insertPark($dbc);
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -9,7 +32,7 @@
 <?php require_once('../views/navbar.php') ?>
 
 <div class= "form_ads">
-<form>
+<form method= "POST">
 <input type="text" id="name" name="username" placeholder="Item Name">
 <br>
 
@@ -21,6 +44,7 @@
 
 <input type="text" id="contact" name="contact" placeholder="Contact Info.">
 <br>
+<input type="submit" value="add">
 </form>
 </div>
 	
