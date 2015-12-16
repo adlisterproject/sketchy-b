87ccdba88ca13d02643745c03dbf8e51a952dec1
Alejandra Ramirez <aer3934@gmail.com>
2015-12-16 12:08:36 -0600

    Ads CREATE

--- models/Ad.php
+++ models/Ad.php
@@ -5,15 +5,15 @@ class Ad extends BaseModel{
 	protected static $table = 'ads_list';
 
 	protected function insert(){
-	$insert = "INSERT INTO " . static::$table . " (item_name, price, description, contact)
-	VALUES (:item_name, :price, :description, :contact)";
-	$stmt = self::$dbc->prepare($insert);
-	$stmt->bindValue(':item_name', $this->attributes['item_name'], PDO::PARAM_STR);
-	$stmt->bindValue(':price', $this->attributes['price'], PDO::PARAM_STR);
-	$stmt->bindValue(':description', $this->attributes['description'], PDO::PARAM_STR);
-	$stmt->bindValue(':contact', $this->attributes['contact'], PDO::PARAM_STR);
-	$stmt->execute();
-}
+		$insert = "INSERT INTO " . static::$table . " (item_name, price, description, contact)
+		VALUES (:item_name, :price, :description, :contact)";
+		$stmt = self::$dbc->prepare($insert);
+		$stmt->bindValue(':item_name', $this->attributes['item_name'], PDO::PARAM_STR);
+		$stmt->bindValue(':price', $this->attributes['price'], PDO::PARAM_STR);
+		$stmt->bindValue(':description', $this->attributes['description'], PDO::PARAM_STR);
+		$stmt->bindValue(':contact', $this->attributes['contact'], PDO::PARAM_STR);
+		$stmt->execute();
+	}
 }
 
 
