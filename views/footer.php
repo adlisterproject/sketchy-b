94e50a4d50769475b9020fb76e34b686b119df29
Mary Warren <Friday@Marys-MacBook-Air.local>
2016-01-04 09:16:42 -0600

    forgot what i added. fixed some bug.

--- views/footer.php
+++ views/footer.php
@@ -1,3 +1,3 @@
-<script src="../../public/js/bootstrap.min.js"></script>
-<script src="../../public/js/jquery-1.11.3.min.js"></script>
-<script src = "../../public/js/main.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script src="/js/jquery-1.11.3.min.js"></script>
+<script src = "/js/main.js"></script>
